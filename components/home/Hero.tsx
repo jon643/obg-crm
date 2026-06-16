1e02496954bc0add2b5ef128cbc1974dd2b969d9
jon643 <jon@onlinebrandgrowth.com>
2026-06-16 16:23:06 -0400

    Update Hero.tsx

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -51,7 +51,7 @@ export default function Hero() {
                 eventName="book_call_click"
                 eventParams={{ cta_location: 'hero' }}
               >
-                Get Your FREE OBG360 Amazon Account Audit
+                Get Your FREE OBG360 Audit
               </TrackedLink>
               <Link href="/case-studies" className="btn-secondary text-lg px-9 py-4">
                 View Case Studies
